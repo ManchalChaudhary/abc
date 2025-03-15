71a75e619b2f1b3f05586dd81828820184b9dfe0
manchal <manchalc@gmail.com>
2025-03-14 19:50:07 -0500

    class1

---
+++ src/helloWord/Xyz.java
@@ -0,0 +1,13 @@
+package helloWord;
+
+public class Xyz {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		System.out.println("hello1");
+		
+		
+	}
+
+}
---
+++ src/helloWord/Xyz1.java
@@ -0,0 +1,12 @@
+//Name: Manchal
+//Date: 2/15/25
+//Details: hello word
+package helloWord;
+
+public class Xyz1 {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome");
+
+	}
+}
